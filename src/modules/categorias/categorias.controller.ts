8a483d5093aecd5f7bceffca2f42b84c4e983db8
.-.. .- . .-. - . <laertelai12@gmail.com>
2026-06-03 15:37:31 -0300

    Criando Rotas para meu modulo de categorias
    Adicionadas rotas CRUD para categorias: create, read, update, delete
    
    Implementadas rotas de busca por id, nome e datas: createdAt, updatedAt, updatedAfter, updatedBetween
    
    Criada rota para adicionar livro em uma categoria usando DTO
    
    Estruturado uso de DTOs CreateCategoriaDto, UpdateCategoriaDto, ValidationBookDto

--- src/modules/categorias/categorias.controller.ts
+++ src/modules/categorias/categorias.controller.ts
@@ -1,4 +1,79 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Put, Patch, Delete, Body, Param, Post, Query} from '@nestjs/common';
+import { CategoriasService } from './categorias.service.js';
+import { CreateCategoriaDto } from './dtos/create-categorias.dto.js';
+import { UpdateCategoriaDto } from './dtos/update-categorias.dto.js';
+import { ValidationBookDto } from './dtos/validation-book.dto.js';
 
 @Controller('categorias')
-export class CategoriasController {}
+export class CategoriasController {
+  constructor(private readonly CategoriaService: CategoriasService){}
+
+  @Post()
+  async create(@Body() dto: CreateCategoriaDto){
+    return this.CategoriaService.create(dto);
+  }
+
+  @Post(':nome/adicionarLivro/')
+  async addBookCategory(@Body() dto: ValidationBookDto, @Param('nome') categoriaNome: string){
+    return this.CategoriaService.addBookCategory(dto,categoriaNome);
+  }
+  
+  @Patch(':nome/removerLivro/')
+  async deleteBookCategory(@Param() dto: ValidationBookDto,categoriaNome: string ){
+    return this.CategoriaService.deleteBookCategory(dto,categoriaNome);
+  }
+
+  @Patch(':nome')
+  async updateByName(@Param('nome') nomeAntigo: string, @Body() dto: UpdateCategoriaDto){
+    return this.CategoriaService.updateByName(nomeAntigo, dto);
+  }
+
+  @Patch(':id')
+  async updateById(@Param('id') id: string, @Body() dto: UpdateCategoriaDto){
+    return this.CategoriaService.updateById(id, dto);
+  } 
+  @Delete(':nome')
+  async deleteByName(@Param('nome') nome: string) {
+    return this.CategoriaService.deleteByName(nome);
+  }
+
+  @Delete(':id')
+  async deleteById(@Param('id') id: string) {
+    return  this.CategoriaService.deleteById(id);
+  }
+
+  @Get()
+  async findAll(){
+    return this.CategoriaService.findAll();
+  }
+
+  @Get(':id')
+  async findId(@Param('id')id: string){ 
+    return this.CategoriaService.findId(id);
+  }
+
+  @Get(':nome')
+  async findName(@Param('nome') nome: string){
+    return this.CategoriaService.findName(nome);
+  }
+
+  @Get('/findCreatedAt/:date')
+  async findCreatedAt(@Param('date') createdAt: Date){
+    return this.CategoriaService.findCreatedAt(createdAt);
+  }
+  
+  @Get('/findUpdateAt/:date')
+  async findUpdateAt(@Param('date') updateAt: Date){
+    return this.CategoriaService.findUpdateAt(updateAt);
+  }
+
+  @Get('/findUpdatedAfter/:date')
+  async findUpdatedAfter(@Param('date') updateAt: Date){
+    return this.CategoriaService.findUpdatedAfter(updateAt);
+  }
+
+  @Get('/findUpdatedBetween/')
+  async findUpdatedBetween(@Query('start') start: Date, @Query('end') end: Date){
+    return this.CategoriaService.findUpdatedBetween(start, end);
+  }
+}
