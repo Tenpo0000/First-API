126e4ed00bfb0ebedadb5931e9cd2d32dd68f3ea
.-.. .- . .-. - . <laertelai12@gmail.com>
2026-06-05 16:23:37 -0300

    Atualização do método Get em autores Controller
    agora o metodo get pode tanto localizar por ID, Nome, e se nada for informado nos parâmetros ele retornara todos

--- src/modules/autores/autores.controller.ts
+++ src/modules/autores/autores.controller.ts
@@ -12,53 +12,57 @@ export class AutoresController {
     return this.autoresService.create(dto)
   }
 
-  @Delete('/deleteByName/:nome')
+  @Delete('deleteByName/:nome')
   async deleteByName(@Param('nome') nome: string){
     return this.autoresService.deleteByName(nome)
    }
 
-  @Delete('/deleteById/:id')
+  @Delete('deleteById/:id')
   async deleteById(@Param('id') id: string){
     return this.autoresService.deleteById(id)
   }
 
-  @Patch('/updateByName/:nome')
+  @Patch('updateByName/:nome')
   async updateByName(@Param('nome') nome: string, @Body() dto: UpdateAutores){
     return this.autoresService.updateByName(nome, dto)
   }
 
-  @Patch('/updateById/:id')
+  @Patch('updateById/:id')
   async updateById(@Param('id') id: string, @Body() dto: UpdateAutores){
     return this.autoresService.updateById(id, dto)
   }
 
   @Get()
-  async findAll(@Query('nome') nome?: string){  
-    if(nome){return this.autoresService.findByName(nome)}
+  async findAll(@Query('nome') nome?: string, @Query('id') id?: string){  
+    if(nome){
+      return this.autoresService.findByName(nome)
+    } else if(id){
+      return this.autoresService.findById(id)
+    }
     return this.autoresService.findAll()
   }
 
-  @Get('/findCreatedOnDay/:data')
+  @Get('findCreatedOnDay/:data')
   async findCreatedOnDay(@Param('data') data: string){
     return this.autoresService.findCreatedOnDay(new Date(data))
   }
 
-  @Get('/findUpdatedOnDay/:data')
+  @Get('findUpdatedOnDay/:data')
   async findUpdatedOnDay(@Param('data') data: string){
     return this.autoresService.findUpdatedOnDay(new Date(data))
   }
 
-  @Get('/findUpdatedAfter/:data')
+  @Get('findUpdatedAfter/:data')
   async findUpdatedAfter(@Param('data') data: string){
     return this.autoresService.findUpdatedAfter(new Date(data))
   }
 
-  @Get('/findUpdatedBefore/:data')
+  @Get('findUpdatedBefore/:data')
   async findUpdatedBefore(@Param('data') data: string){
     return this.autoresService.findUpdatedBefore(new Date(data))
   }
 
-  @Get('/findUpdatedBetween/:start/:end')
+  @Get('findUpdatedBetween/:start/:end')
   async findUpdatedBetween(@Param('start') start: string,@Param('end') end: string){
     return this.autoresService.findUpdatedBetween(new Date(start), new Date(end))
   }
